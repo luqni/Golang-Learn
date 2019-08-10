a648d6013972be8f0d87149b1c415aae78cf7fe7
luqni <mluqnibaehaqi@gmail.com>
2019-08-10 22:53:37 +0700

    add description

--- go_variable.go
+++ go_variable.go
@@ -1,3 +1,10 @@
+/*
+Program Name : First Go Program
+Version : 1.0
+Description : Go Program basic program structure.
+Author : Muhammad Luqni Baehaqi
+Date Create : 10-08-2019
+*/
 package main
 import "fmt"
 
