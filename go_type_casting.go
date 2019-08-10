fb5f6e22b3a931bd5638117334ad6adafc50649b
luqni <mluqnibaehaqi@gmail.com>
2019-08-11 02:29:44 +0700

    new update

---
+++ go_type_casting.go
@@ -0,0 +1,19 @@
+/*
+Program Name : First Go Program
+Version : 1.0
+Description : Go Program basic program structure.
+Author : Muhammad Luqni Baehaqi
+Date Create : 10-08-2019
+*/
+package main
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	var x,y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x,y,z)
+}
